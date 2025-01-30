298b208e5110c00d9e4ba1c66e18003467e04e8f
William <wihe0001@stud.kea.dk>
2025-01-30 11:50:48 +0100

    dynamisk product side

---
+++ js/product.js
@@ -0,0 +1,31 @@
+let productId = 1590
+let productContainer = document.querySelector(".product_container")
+
+
+
+fetch(`https://kea-alt-del.dk/t7/api/products/${productId}`)
+.then(response => response.json())
+.then(data => {
+    productContainer.innerHTML = `
+    <div>
+          <img src="https://kea-alt-del.dk/t7/images/webp/640/${productId}.webp" alt="Fodboldsko" />
+        </div>
+
+        <div class="product_details_container">
+          <h3 class="h3_product">${data.productdisplayname}</h3>
+
+          <div>${data.price}</div>
+
+          <div>${data.basecolour}</div>
+
+          <div class="size-container">
+            <div>SIZE:</div>
+            <div class="size-menu">NA</div>
+          </div>
+
+          <div class="buy_button">ADD TO BASKET</div>
+
+          <div>ESTIMATED DELIVERY TIME: 1-2 BUSINESS DAYS</div>
+        </div>
+    `
+})
