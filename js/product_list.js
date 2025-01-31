cd266850b2622d925039f550965c3159d3fbc0a9
William <wihe0001@stud.kea.dk>
2025-01-31 10:22:37 +0100

    dynamisk step 1

---
+++ js/product_list.js
@@ -0,0 +1,27 @@
+let listContainer = document.querySelector("main")
+
+
+fetch(`https://kea-alt-del.dk/t7/api/products/`)
+.then((response) => response.json())
+.then((data) => showList(data));
+
+function showList(products){
+    console.log(products);
+    let markup = "";
+    products.map(product=>{
+        markup += `<article class="product_list_container">
+        <a href="product.html">
+          <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="Fodboldsko" />
+          <h3>${product.productdisplayname}</h3>
+        </a>
+        <div class="product_info">
+          <div>PRICE</div>
+          <div class="read_more"><a href="">READ MORE</a></div>
+        </div>
+      </article>`;
+    })
+    .join("");
+console.log(markup);
+
+listContainer.innerHTML = markup;
+}
